d903fd4df5eb25b6ee498bafd6453038eda268f3
meganbeek98 <meganbeek98@gmail.com>
2022-04-08 17:10:54 -0400

    new repo initial commit
    Started over from "code-quiz" repo (it did not work how I needed it to)
    "code-quiz-2" repo is my second attempt, with a different approach.

---
+++ assets/js/questions.js
@@ -0,0 +1,72 @@
+var questions = [
+    {
+        title: "What is something JavaScript is used for?",
+        choices: [
+            "It is used to lay out the basic skeleton of a webpage.",
+            "It is used solely for styling the way a webpage looks.",
+            "It is used to add interactive behavior to webpages.",
+            "none of the above"
+        ],
+        answer: "It is used to add interactive behavior to webpages."
+
+    },
+    {
+        title: "Which is NOT one of the 7 main data types in JavaScript?",
+        choices: [
+            "summary",
+            "strings",
+            "numbers",
+            "objects"
+        ],
+        answer: "summary"
+    },
+    {
+        title: "Which JavaScript data type includes true/false statements?",
+        choices: [
+            "array",
+            "number",
+            "undefined",
+            "boolean"
+        ],
+        answer: "boolean"
+    },
+    {
+        title: "'You can use single OR double quotes when writing strings in JavaScript.' True or False?",
+        choices: [
+            "true",
+            "false"
+        ],
+        answer: "true"
+    },
+    {
+        title: "Who created the first ever JavaScript?",
+        choices: [
+            "Larry Page",
+            "Brendon Eich",
+            "Mark Zuckerberg",
+            "Bill Gates"
+        ],
+        answer: "Brendon Eich"
+    },
+    {
+        title: "What does DOM stand for in JavaScript?",
+        choices: [
+            "Descriptive Option Models",
+            "Document Object Mode",
+            "Document Object Model",
+            "Dynamic Object Modes",
+        ],
+        answer: "Document Object Model"
+    },
+    {
+        title: "What does API stand for in JavaScript?",
+        choices: [
+            "Advanced Programming [tegers",
+            "Advanced Placement Interfaces",
+            "Application Programming Interfaces",
+            "Application Placement Integers"
+        ],
+        answer: "Application Programming Interfaces"
+    }
+    
+];
